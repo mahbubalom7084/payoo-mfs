983aef27ee589e7a8a163e5e432a8c7372516ea3
mahbubalom7084 <mahbubalom7084@gmail.com>
2025-07-11 03:26:38 +0600

    added cashout.js

--- script/cashout.js
+++ script/cashout.js
@@ -1,3 +1,17 @@
 document.getElementById("cashout-btn").addEventListener('click',function(event){
     event.preventDefault()
+    const cashOutAmount= document.getElementById('cash-out-amount').value;
+    const convertedAmount = parseFloat(cashOutAmount);
+    const pin = document.getElementById('pin').value;
+    const convertedPin = parseInt(pin)
+    const mainBlance = document.getElementById('main-balance').innerText;
+    const convertedMainBalance = parseFloat(mainBlance);
+
+        if(convertedPin===1234){
+            const minusAmount =convertedAmount - convertedMainBalance;
+            document.getElementById('main-balance').innerText = minusAmount;
+        }
+        else{
+            alert("Please enter valid pin")
+        }
 })
