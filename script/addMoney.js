9d0c4283d2628d455c42ddf222d932f50a1d0cb7
mahbubalom7084 <mahbubalom7084@gmail.com>
2025-07-11 00:31:51 +0600

    added addMoney.js

---
+++ script/addMoney.js
@@ -0,0 +1,18 @@
+document.getElementById('addMoney-btn')
+.addEventListener('click',function(event){
+    event.preventDefault();
+    const ammount= document.getElementById("ammount").value;
+    const convertAmmount= parseFloat(ammount);
+    const pin= document.getElementById("pin").value;
+    const convertPin= parseInt(pin);
+    const mainBalance= document.getElementById("main-balance").innerText;
+    const convertMainBalance=parseFloat(mainBalance);
+
+    if(convertPin===1234){
+        const sum= convertMainBalance + convertAmmount;
+        document.getElementById('main-balance').innerText=sum;
+    }
+    else{
+        console.log("invalid pin")
+    }
+})
