1fd9c60bd5f95322d90847805bacfff99cf7b043
mahbubalom7084 <mahbubalom7084@gmail.com>
2025-07-11 02:58:23 +0600

    added toggle pblm solving part

--- script/toggle.js
+++ script/toggle.js
@@ -1,9 +1,12 @@
-document.getElementById('addMoney-box').addEventListener('click',function(){
-    document.getElementById('add-money').style.display='block';
-    document.getElementById('cash-out').style.display='none';
-})
+document.getElementById("cash-out").style.display = "block";
+document.getElementById("add-money").style.display = "none";
 
-document.getElementById('cashout-box').addEventListener('click',function(){
-    document.getElementById('cash-out').style.display='block';
-    document.getElementById('add-money').style.display='none';
-})
+document.getElementById("addMoney-box").addEventListener("click", function () {
+  document.getElementById("add-money").style.display = "block";
+  document.getElementById("cash-out").style.display = "none";
+});
+
+document.getElementById("cashout-box").addEventListener("click", function () {
+  document.getElementById("cash-out").style.display = "block";
+  document.getElementById("add-money").style.display = "none";
+});
