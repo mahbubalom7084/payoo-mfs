10eab3632d128d690ddd3f17a18040a548a3cc81
mahbubalom7084 <mahbubalom7084@gmail.com>
2025-07-10 22:52:04 +0600

    added login js file

--- script/login.js
+++ script/login.js
@@ -1,4 +1,19 @@
-document.getElementById('login-btn')
-.addEventListener('click',function(){
-    // console.log('hello');
-})
+document.getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const accountNumber = document.getElementById("Account-number").value;
+    const pin = document.getElementById("pin").value;
+
+    const convertAccountNumber = parseInt("accountNumber");
+    const convertPinNumber = parseInt(pin);
+
+    if (accountNumber.length === 11) {
+      if (convertPinNumber === 1234) {
+        window.location.href="./main.html"
+      } else {
+        alert("invalid pin number");
+      }
+    } else {
+      alert("invalid account number");
+    }
+  });
