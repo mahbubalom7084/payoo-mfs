89804f6ffdac7180430702d9f8d0555b35227136
mahbubalom7084 <mahbubalom7084@gmail.com>
2025-07-16 22:53:28 +0600

    added toggle

--- script/toggle.js
+++ script/toggle.js
@@ -1,5 +1,9 @@
-document.getElementById("cash-out").style.display = "block";
-document.getElementById("add-money").style.display = "none";
+document.getElementById("add-money").style.display = "block";
+document.getElementById("cash-out").style.display = "none";
+document.getElementById("Transfer-Money").style.display = "none";
+document.getElementById("Get-Bonus").style.display = "none";
+document.getElementById("Pay-Bill").style.display = "none";
+document.getElementById("Transactions-History").style.display = "none";
 
 document.getElementById("addMoney-box").addEventListener("click", function () {
   document.getElementById("add-money").style.display = "block";
