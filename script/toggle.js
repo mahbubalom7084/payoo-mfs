fce9cc89b4977a803e58d5abce8785d210350e09
mahbubalom7084 <mahbubalom7084@gmail.com>
2025-07-19 23:53:23 +0600

    all file submited

--- script/toggle.js
+++ script/toggle.js
@@ -14,3 +14,5 @@ document.getElementById("cashout-box").addEventListener("click", function () {
   document.getElementById("cash-out").style.display = "block";
   document.getElementById("add-money").style.display = "none";
 });
+
+document.getElementById
